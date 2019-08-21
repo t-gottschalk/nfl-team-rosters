98b3a0a9a25de37f082eb973f900f086c29e969c
code-and-technology <tony.gottschalk@alumni.unc.edu>
2019-08-20 21:07:08 -0400

    Updates

---
+++ precache-manifest.0d5d749934b510af8beccd8ae2d22545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72fc7f1f84d82319bf90db11f2c8231",
+    "url": "/nfl-team-rosters/index.html"
+  },
+  {
+    "revision": "75fe3e910c6a1a8306a6",
+    "url": "/nfl-team-rosters/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "28373f984d771d425c65",
+    "url": "/nfl-team-rosters/static/css/main.e3fdb487.chunk.css"
+  },
+  {
+    "revision": "75fe3e910c6a1a8306a6",
+    "url": "/nfl-team-rosters/static/js/2.2d781f7c.chunk.js"
+  },
+  {
+    "revision": "28373f984d771d425c65",
+    "url": "/nfl-team-rosters/static/js/main.e97c5c6e.chunk.js"
+  },
+  {
+    "revision": "b25930c93cf63f0daf8a",
+    "url": "/nfl-team-rosters/static/js/runtime~main.c0e9efb5.js"
+  }
+]);
